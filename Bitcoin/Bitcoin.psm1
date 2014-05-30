596ece049571cc2b5975a0a066ea5aea6a5215f4
Joe Levy <jodoglevy@gmail.com>
2014-05-30 16:02:23 -0700

    Update Bitcoin.psm1

--- Bitcoin/Bitcoin.psm1
+++ Bitcoin/Bitcoin.psm1
@@ -1,45 +1,27 @@
 ﻿function Get-BitcoinPrices {
     $exchanges = @()
-
-    $exchanges += getBitstampPrices
+    
     $exchanges += getBtc-ePrices
     $exchanges += getCampBxPrices
     $exchanges += getCoinbasePrices
-    $exchanges += getGoxPrices
 
     $exchanges
 }
 
-function getBitstampPrices() {
-    $request = Invoke-WebRequest -Uri https://www.bitstamp.net/api/ticker/
-    $json = ConvertFrom-Json $request.Content
-    new-object psobject -property  @{BuyPrice = $json.Ask; SellPrice = $json.Bid; Exchange = "Bitstamp"}
-}
-
 function getBtc-ePrices() {
-    $request = Invoke-WebRequest -Uri https://btc-e.com/api/2/btc_usd/ticker
-    $json = ConvertFrom-Json $request.Content
-    new-object psobject -property  @{BuyPrice = $json.ticker.buy; SellPrice = $json.ticker.sell; Exchange = "BTC-E"}
+    $response = Invoke-RestMethod -Uri https://btc-e.com/api/2/btc_usd/ticker
+    new-object psobject -property  @{BuyPrice = $response.ticker.buy; SellPrice = $response.ticker.sell; Exchange = "BTC-E"}
 }
 
 function getCampBxPrices() {
-    $request = Invoke-WebRequest -Uri http://campbx.com/api/xticker.php
-    $json = ConvertFrom-Json $request.Content
-    new-object psobject -property  @{BuyPrice = $json."Best Ask"; SellPrice =  $json."Best Bid"; Exchange = "Camp BX"}
+    $response = Invoke-RestMethod -Uri http://campbx.com/api/xticker.php
+    new-object psobject -property  @{BuyPrice = $response."Best Ask"; SellPrice =  $response."Best Bid"; Exchange = "Camp BX"}
 }
 
 function getCoinbasePrices() {
-    $buy = Invoke-WebRequest -Uri https://coinbase.com/api/v1/prices/buy
-    $sell = Invoke-WebRequest -Uri https://coinbase.com/api/v1/prices/sell
-    $jsonBuy = ConvertFrom-Json $buy.Content
-    $jsonSell = ConvertFrom-Json $sell.Content
-    new-object psobject -property  @{BuyPrice = $jsonBuy.total.amount; SellPrice = $jsonSell.total.amount; Exchange = "Coinbase"}
-}
-
-function getGoxPrices() {
-    $request = Invoke-WebRequest -Uri http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast
-    $json = ConvertFrom-Json $request.Content
-    new-object psobject -property  @{BuyPrice = $json.data.buy.value; SellPrice = $json.data.sell.value; Exchange = "Mt. Gox"}
+    $buy = Invoke-RestMethod -Uri https://coinbase.com/api/v1/prices/buy
+    $sell = Invoke-RestMethod -Uri https://coinbase.com/api/v1/prices/sell
+    new-object psobject -property  @{BuyPrice = $buy.total.amount; SellPrice = $sell.total.amount; Exchange = "Coinbase"}
 }
 
 Export-ModuleMember Get-BitcoinPrices
